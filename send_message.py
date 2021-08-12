dacd6fbb3c7e72ce22befcf83c665f0842af2afa
min <min.zhang@daocloud.io>
2021-08-12 10:18:28 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -3,8 +3,13 @@ import os
 
 
 def send_message():
+    
+    if os.getenv("WEIXIN_URL") is None:
+        url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=62002e06-2ebf-41c3-a17f-b99b31b284c2"
+    else:
+        url = os.getenv("WEIXIN_URL")
 
-    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=62002e06-2ebf-41c3-a17f-b99b31b284c2"  # test 机器人
+    # url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=62002e06-2ebf-41c3-a17f-b99b31b284c2"  # test 机器人
     # url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7e39ce84-77c9-4882-b637-314a8ceb52bd"  # dce406机器人
     header = {
         "Content-Type": "application/json"
