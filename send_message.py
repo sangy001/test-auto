6ea74bad28ed2d0e39cfa63874a001270f4fac86
min <min.zhang@daocloud.io>
2021-08-12 10:14:24 +0800

    Add new file

---
+++ send_message.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+
+def send_message():
+
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=62002e06-2ebf-41c3-a17f-b99b31b284c2"  # test 机器人
+    # url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7e39ce84-77c9-4882-b637-314a8ceb52bd"  # dce406机器人
+    header = {
+        "Content-Type": "application/json"
+    }
+
+    body = {
+        "msgtype": "markdown",
+        "markdown": {
+            "content":
+                f"DX-ARCH已经安装完成\n" 
+            }
+    }
+    response = requests.post(url=url, json=body, headers=header)
+    print(response.content)
+
+
+if __name__ == "__main__":
+    # initLogging()
+    print("run")
+    send_message()
